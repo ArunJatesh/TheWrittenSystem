83379f7c3ca6a4037e1fab0ce7f822a1a0365776
Jatesh <ganeshsai25@gmail.com>
2021-03-15 16:03:52 +0530

    Basic Navigation Added

--- app/src/main/java/com/example/thewrittensystem/MainActivity.java
+++ app/src/main/java/com/example/thewrittensystem/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.thewrittensystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TextView textView = (TextView) findViewById(R.id.textview);
+
+        //Typeface typeface = ResourcesCompat.getFont(this, R.font.mukta_malar_regular);
+
+        //textView.setText("வணக்கம்");
+
+        //textView.setTypeface(typeface);
     }
 }
